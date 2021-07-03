109291f39cde4e45dae018fc43502a471d2839fd
ansuman273 <ansuman273@gmail.com>
2021-07-03 16:00:17 +0530

    refractor of javascript TennisGame1

--- javascript/README.md
+++ javascript/README.md
@@ -6,3 +6,9 @@ For node, simply call
 (Only failures will be shown on the console)
 
 For the browser, open TennisTest.html and refresh after change.
+
+# Refractor Imrovements
+
+1. Added JsDoc for author comments and documentation which enhances the overall readability and makes it easy maintainable code.
+
+2. Made use of standard camelCase format for all the variable and function names to adhere the JavaScript best practices for naming convention. Results indicate that camel casing leads to higher accuracy among all subjects regardless of training, and those trained in camel casing are able to recognize identifiers in the camel case style faster than identifiers in the underscore style.
--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,22 +1,69 @@
+/**
+ * Constructor used to initialize or create a new game
+ * @constructor
+ * @date 2021-07-03
+ * @param {any} player1Name
+ * @param {any} player2Name
+ */
 var TennisGame1 = function(player1Name, player2Name) {
-    this.m_score1 = 0;
-    this.m_score2 = 0;
+    /**
+     * Score of player1.
+     * @name TennisGame1#player1Score
+     * @type {any}
+     */
+    this.player1Score = 0;
+    /**
+     * Score of player2.
+     * @name TennisGame1#player2Score
+     * @type {any}
+     */
+    this.player2Score = 0;
+    /**
+     * Name of player1.
+     * @name TennisGame1#player1Name
+     * @type {any}
+     */
     this.player1Name = player1Name;
+    /**
+     * Name of player2.
+     * @name TennisGame1#player2Name
+     * @type {any}
+     */
     this.player2Name = player2Name;
 };
 
+/**
+ *  Tennis Point calculation
+ * @date 2021-07-03
+ * @param {any} playerName
+ */
 TennisGame1.prototype.wonPoint = function(playerName) {
     if (playerName === "player1")
-        this.m_score1 += 1;
+        this.player1Score += 1;
     else
-        this.m_score2 += 1;
+        this.player2Score += 1;
 };
 
+/**
+ * Get current game scores
+ * @date 2021-07-03
+ * @returns {any} score
+ */
 TennisGame1.prototype.getScore = function() {
+    /**
+     * Score Details.
+     * @name getScore#score
+     * @type {any}
+     */
     var score = "";
+    /**
+     * Temporary score variable.
+     * @name getScore#tempScore
+     * @type {any}
+     */
     var tempScore = 0;
-    if (this.m_score1 === this.m_score2) {
-        switch (this.m_score1) {
+    if (this.player1Score === this.player2Score) {
+        switch (this.player1Score) {
             case 0:
                 score = "Love-All";
                 break;
@@ -30,18 +77,18 @@ TennisGame1.prototype.getScore = function() {
                 score = "Deuce";
                 break;
         }
-    } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
-        var minusResult = this.m_score1 - this.m_score2;
+    } else if (this.player1Score >= 4 || this.player2Score >= 4) {
+        var minusResult = this.player1Score - this.player2Score;
         if (minusResult === 1) score = "Advantage player1";
         else if (minusResult === -1) score = "Advantage player2";
         else if (minusResult >= 2) score = "Win for player1";
         else score = "Win for player2";
     } else {
         for (var i = 1; i < 3; i++) {
-            if (i === 1) tempScore = this.m_score1;
+            if (i === 1) tempScore = this.player1Score;
             else {
                 score += "-";
-                tempScore = this.m_score2;
+                tempScore = this.player2Score;
             }
             switch (tempScore) {
                 case 0:
